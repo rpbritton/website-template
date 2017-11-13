188451af40875550a3e30178b0d83bea9fead024
rpbritton <rpbritton@outlook.com>
2017-11-12 21:39:44 -0500

    added content-img

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,4 @@
 for (let button of document.getElementsByClassName('sidebar-button')) {
 	button.addEventListener('mousedown', function(ev) { inkDropStart(ev, this, this, colors.bg); });
-	button.addEventListener('mouseup', function(ev) { inkDropEnd(); });
-	button.addEventListener('mouseleave', function(ev) { inkDropEnd(); });
+	button.addEventListener('dragend', function(ev) { inkDropEnd(); });
 }
