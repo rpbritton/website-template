af8f8f0c717017169d7150c692a19a9277fb427e
rpbritton <rpbritton@outlook.com>
2017-11-13 19:44:26 -0500

    finished content-img; create a base for content-cards

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,4 @@
 for (let button of document.getElementsByClassName('sidebar-button')) {
-	button.addEventListener('mousedown', function(ev) { inkDropStart(ev, this, this, colors.bg); });
+	button.addEventListener('mousedown', function(ev) { inkDropStart(ev, this, this, colors.fd); });
 	button.addEventListener('dragend', function(ev) { inkDropEnd(); });
 }
--- js/theme.js
+++ js/theme.js
@@ -12,6 +12,7 @@
 }*/
 
 const colors = {
-	'fg': window.getComputedStyle(document.body).getPropertyValue("--fg"),
-	'bg': window.getComputedStyle(document.body).getPropertyValue('--bg')
+	'fg': window.getComputedStyle(document.body).getPropertyValue('--fg'),
+	'bg': window.getComputedStyle(document.body).getPropertyValue('--bg'),
+	'fd': window.getComputedStyle(document.body).getPropertyValue('--fd')
 };
