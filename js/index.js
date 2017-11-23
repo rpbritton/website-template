963e60ceabf0a5db79ab427e97019413f3517ccb
rpbritton <rpbritton@outlook.com>
2017-11-23 13:15:23 -0500

    cleaned up

--- js/index.js
+++
@@ -1,6 +0,0 @@
-/*const colors = {
-	'fg': window.getComputedStyle(document.body).getPropertyValue('--fg'),
-	'bg': window.getComputedStyle(document.body).getPropertyValue('--bg'),
-	'fd': window.getComputedStyle(document.body).getPropertyValue('--fd'),
-	'l': window.getComputedStyle(document.body).getPropertyValue('--l')
-};*/
